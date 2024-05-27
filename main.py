da59d2e01c5f86c911ab33052bd4294d3dc69b55
Supersrimanth007 <140915047+Supersrimanth007@users.noreply.github.com>
2024-05-28 01:13:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import base64
 st.sidebar.title("Titanic 🚢 Survival Prediction")
 
 # Pclass
-Pclass = st.sidebar.selectbox("Passenger Class (Pclass)", (1, 2, 3))
+Pclass = st.sidebar.selectbox("Passenger Class", (1, 2, 3))
 
 # Sex
 Sex = st.sidebar.selectbox("Sex", ("Male", "Female"))
@@ -15,17 +15,17 @@ Sex_dict = {'Male': 0, 'Female': 1}
 Age = st.sidebar.number_input("Age", min_value=0, max_value=100, value=25)
 
 # SibSp
-SibSp = st.sidebar.selectbox("Siblings or Spouses Aboard (SibSp)", ("No", "Yes"))
+SibSp = st.sidebar.selectbox("Siblings or Spouses Aboard", ("No", "Yes"))
 SibSp_dict = {'No': 0, 'Yes': 1}
 
 # Parch
-Parch = st.sidebar.number_input("Parents or Children Aboard (Parch)", min_value=0, max_value=10, value=0)
+Parch = st.sidebar.number_input("Parents or Children Aboard", min_value=0, max_value=10, value=0)
 
 # Fare
 Fare = st.sidebar.number_input("Ticket Fare", min_value=0.0, max_value=500.0, value=32.2)
 
 # Embarked
-Embarked = st.sidebar.selectbox("Port of Embarkation (Embarked)", ("Cherbourg (C)", "Queenstown (Q)", "Southampton (S)"))
+Embarked = st.sidebar.selectbox("Port of Embarkation", ("Cherbourg", "Queenstown", "Southampton"))
 
 # Load the saved model
 predict_button = st.sidebar.button("Predict Survival")
@@ -47,11 +47,11 @@ if predict_button:
         Fare,
     ]
     
-    if Embarked == "Cherbourg (C)":
+    if Embarked == "Cherbourg":
         Features += [0, 0]
-    elif Embarked == "Queenstown (Q)":
+    elif Embarked == "Queenstown":
         Features += [1, 0]
-    else:  # Southampton (S)
+    else:  # Southampton 
         Features += [0, 1]
     
     # Load model
